d9724858eb5df7732df980f39413b4117d0e967c
Adam Kukliński <64474051+Q-Click@users.noreply.github.com>
2020-08-21 13:01:41 +0200

    up

---
+++ build/precache-manifest.b21cb91abdd57ca2c909b0960c3b797b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38e079add0aca2c2eadf",
+    "url": "/Projekt-7---Weather-App/static/css/main.cc071bba.chunk.css"
+  },
+  {
+    "revision": "38e079add0aca2c2eadf",
+    "url": "/Projekt-7---Weather-App/static/js/main.38e079ad.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Projekt-7---Weather-App/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "fb4f364c42da89f57857",
+    "url": "/Projekt-7---Weather-App/static/js/runtime~main.fb4f364c.js"
+  },
+  {
+    "revision": "0086c404a367c1793b08639abda95bb5",
+    "url": "/Projekt-7---Weather-App/static/media/photo-of-mountains-during-sunset.0086c404.jpg"
+  },
+  {
+    "revision": "13c2e74e88c9c25a0a266467b062e4db",
+    "url": "/Projekt-7---Weather-App/index.html"
+  }
+];
